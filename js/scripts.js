0dc8dff0f3eca20e8bdc112b476fd98cc93706aa
Flora <fpeterson1@lab-che204-m10.eastern.ewu.edu>
2019-06-05 11:14:39 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ console.log ('OK')
 // Page Headings
 
 $('h1').lettering();
+
+// Portfolio
+$('.portfolio').chocolat({
+
+    enableZoom : false,
+    separator2 : "- -",
+    fullScreen : true,
+
+});
